38aff807fd4adcb1a8ed4e08cb36024b74adffbd
Rakesh Patel <rakesh.patel1978@gmail.com>
2025-02-04 06:41:44 +0000

    Initial draft by co-pilot

--- MessageQueue.cpp
+++ MessageQueue.cpp
@@ -1,1 +1,34 @@
+#include <queue>
+#include <string>
+#include <mutex>
+#include <condition_variable>
+
+class MessageQueue {
+public:
+    MessageQueue() : max_size(1000) {}
+
+    void enqueue(const std::string& message) {
+        std::unique_lock<std::mutex> lock(mtx);
+        cond_var.wait(lock, [this]() { return queue.size() < max_size; });
+        queue.push(message);
+        cond_var.notify_all();
+    }
+
+    std::string dequeue() {
+        std::unique_lock<std::mutex> lock(mtx);
+        cond_var.wait(lock, [this]() { return !queue.empty(); });
+        std::string message = queue.front();
+        queue.pop();
+        cond_var.notify_all();
+        return message;
+    }
+
+private:
+    std::queue<std::string> queue;
+    std::mutex mtx;
+    std::condition_variable cond_var;
+    const size_t max_size;
+};
+
+// ...existing code...
 
